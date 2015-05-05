423b1724d325ffc81a009df5aa06fa95071d5da1
iToktor <itoktor@gmail.com>
2015-05-05 21:52:51 +0300

    style(header): redesign toolbar header and add tab icons

--- modules/debug/config/toolbar.php
+++ modules/debug/config/toolbar.php
@@ -7,11 +7,15 @@ return array(
                 'messages' => array(
                         'name' => 'Messages',
                         'method' => 'messages',
+                        'logo' => 'img/mail.png',
+                        'position' => 'left',
                         'configs' => array()
                 ),
                 'files' => array(
                         'name' => 'Files',
                         'method' => 'files',
+                        'logo' => 'img/files.png',
+                        'position' => 'left',
                         'configs' => array()
                 )
         ) 
--- templates/toolbar/view/messages.php
+++ templates/toolbar/view/messages.php
@@ -1,6 +1,11 @@
 <ul>
-    <?php foreach ($messages as $message):?>
-        <li><?=$message?></li>
+    <?php foreach ($messages as $key => $message):?>
+    <li class="spoiler">
+        <input type="checkbox" id="id_<?=$key?>">
+        <label for="id_<?=$key?>"><?=$message['header']?></label>
+        <?php if(isset($message['body'])):?>
+        <div class="spoiler-body"><?=$message['body']?></div>
+        <?php endif;?>
+    </li>
     <?php endforeach;?>
 </ul>
-
